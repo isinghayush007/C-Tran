e366ea05b94632b29e5f7b947e7b691059650287
Ayush <ayush474020@gmail.com>
2024-04-15 11:44:32 +0530

    1

--- config_args.py
+++ config_args.py
@@ -14,7 +14,7 @@ def get_args(parser,eval=False):
     # Optimization
     parser.add_argument('--optim', type=str, choices=['adam', 'sgd'], default='adam')
     parser.add_argument('--lr', type=float, default=0.0002)
-    parser.add_argument('--batch_size', type=int, default=16)
+    parser.add_argument('--batch_size', type=int, default=8)
     parser.add_argument('--test_batch_size', type=int, default=-1)
     parser.add_argument('--grad_ac_steps', type=int, default=1)
     parser.add_argument('--scheduler_step', type=int, default=1000)
